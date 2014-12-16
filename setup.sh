af10bfc6bb9b3ebd3f3ef2e43014f94f5c96373a
Pawit Khid-arn <khidarn.pawit@gmail.com>
2014-12-16 19:20:07 +0700

    change setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
+if [ -e ~/.vimrc ]; then mv ~/.vimrc ~/.vimrc_bak; fi
+if [ -e ~/.vim ]; then mv ~/.vim ~/.vim_bak; fi
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-cp -rf ./dotfiles/* ~/
+cp ./dotfiles/.* ~/
