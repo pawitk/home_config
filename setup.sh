a40a288dd5d6c2c3a4ca62442eed1140ae91ab4f
Pawit Khid-arn <khidarn.pawit@gmail.com>
2014-12-20 08:51:54 +0700

    updated tmux.conf and setup files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,21 @@
+# backup dotfiles and folders
 if [ -e ~/.vimrc ]; then mv ~/.vimrc ~/.vimrc_bak; fi
 if [ -e ~/.vim ]; then mv ~/.vim ~/.vim_bak; fi
+if [ -e ~/.tmux.conf ]; then mv ~/.tmux.conf ~/.tmux.conf_bak; fi
+if [ -e ~/.tmux ]; then mv ~/.tmux ~/.tmux_bak; fi
+
+# clone plugin managers
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# install tmux plugins from command line
+tmux start-server
+tmux new-session -d
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+tmux kill-server
+
+# install vundle plugins from command line
+vim +PluginInstall +qall
+
+# copy new dotfiles to home directory
 cp ./dotfiles/.* ~/
