6cfd7c32c56dedfc1c59cd46e5b27cfe692921d8
Pawit Khid-arn <khidarn.pawit@gmail.com>
2014-12-20 09:07:18 +0700

    added remove script

---
+++ remove.sh
@@ -0,0 +1,4 @@
+if [ -e ~/.vimrc ]; then rm ~/.vimrc; fi                                                                                                                   
+if [ -e ~/.vim ]; then rm ~/.vim; fi                                                                                                                         
+if [ -e ~/.tmux.conf ]; then rm ~/.tmux.conf; fi                                                                                                       
+if [ -e ~/.tmux ]; then rm ~/.tmux; fi 
